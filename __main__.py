a8f067dbf4c6ddff91c04d051929deb952aef66a
TheZakMan <TheZakMan@ctf-br.org>
2025-05-13 12:48:26 -0300

    1.2.4

--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@ Allows running the package as a module.
 Example: python -m LeftOvers -u http://www.example.com
 """
 
-from LeftOvers.core.cli import main
+from core.cli import main
 
 if __name__ == "__main__":
     main()
