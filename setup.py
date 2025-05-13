3529212f9353e5402644e477623773423ba0028e
TheZakMan <TheZakMan@ctf-br.org>
2025-05-13 12:43:04 -0300

    1.2.4

--- setup.py
+++ setup.py
@@ -1,43 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name="LeftOvers",
-    version="1.1.9",
+    version="1.2.3",  # Updated to match current app_settings.py version
     description="An advanced scanner to find residual files on web servers",
     author="TheZakMan",
-    packages=find_packages(),
+    packages=find_namespace_packages(include=["LeftOvers", "LeftOvers.*"]),
     include_package_data=True,
     install_requires=[
-        "requests>=2.27.1",
-        "colorama>=0.4.4",
-        "rich>=12.0.0",
-        "tqdm>=4.62.3",
-        "urllib3>=1.26.8",
-        "tldextract>=3.1.2",
-        "pyOpenSSL>=22.0.0",
-        "cryptography>=36.0.0",
-        "concurrent-futures-pool>=1.0.0",
+        "requests>=2.31.0",
+        "colorama>=0.4.6",
+        "rich>=13.4.2",
+        "tqdm>=4.66.1",
+        "urllib3>=2.0.4",
+        "tldextract>=3.4.4",
+        "pyOpenSSL>=23.2.0",
+        "cryptography>=41.0.3",
+        "concurrent-futures-pool>=1.1.0",
     ],
     entry_points={
         "console_scripts": [
             "leftovers=LeftOvers.__main__:main",
         ],
     },
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Information Technology",
         "Topic :: Security",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     keywords="security, scanner, web, pentest, residual files, backups",
 )
